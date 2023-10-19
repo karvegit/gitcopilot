a73964bef338aefd11b26e8aa90965f9dde8d875
karvegit <karvenitin@yahoo.co.in>
2023-10-19 18:23:15 +0530

    trying using copilot

--- CoPilotJavaProject/src/main/java/com/copilotjavaproject/Test.java
+++ CoPilotJavaProject/src/main/java/com/copilotjavaproject/Test.java
@@ -1,9 +1,15 @@
 package com.copilotjavaproject;
 
+import javax.lang.model.element.NestingKind;
+
 public class Test {
     //Create a function to reverse a string
         //create a method to reverse a string
 //create a method to add two numbers
-
+//add two numbers
+public static void main (String argv[]){
+    int i = 0 ;
+    i = i + 1;
+}
 
 }
